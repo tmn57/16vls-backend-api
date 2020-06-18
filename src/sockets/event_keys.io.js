249f49d166411a93747a9ff9f40c2fabd985d5bf
tmn57 <minhnhattranw@gmail.com>
2020-06-18 12:44:08 +0700

    update stream event keys

--- src/sockets/event_keys.io.js
+++ src/sockets/event_keys.io.js
@@ -7,25 +7,25 @@
  */
 module.exports = {
     //JSON payload
-    STREAM_INIT: 'stream init',
-    STREAM_PRODUCT_QUANTITY: 'stream updates quantity', //{productIndex, variantIndex, quantity}
-    STREAM_CHAT_MESSAGE: 'stream chat message',
-    USER_ADD_PRODUCT_TO_CART: 'user adds product', //{productIndex,isReliable,variantIndex,quantity}
     SERVER_MESSAGE: 'server message', //{type:"error"/"message", message}
+    STREAM_INIT: 'stream init',
+    STREAM_CHAT_MESSAGE: 'stream chat message',
     STREAM_MESSAGE: 'stream message',
+    STREAM_PRODUCT_QUANTITY: 'stream updates quantity', //{productIndex, variantIndex, quantity}
     STREAM_UPDATE_CURRENT_PRODUCT_INDEX: 'stream updates current product index', //{productIndex,inStreamAt}
     STREAM_STATUS_UPDATE: 'stream status updated', //{statusCode: 0/1/2/3, [videoUri], [message]} ---- 0: incoming stream, 1 is live, 2: was live, 3: error
-    SELLER_UPDATE_STREAMPRICE: 'seller updates streamPrice',// {productIndex, streamPrice},
     STREAM_UPDATE_STREAMPRICE: 'stream updates streamPrice',// {productIndex, streamPrice}
+    SELLER_UPDATE_STREAMPRICE: 'seller updates streamPrice',// {productIndex, streamPrice},
+    USER_ADD_PRODUCT_TO_CART: 'user adds product', //{productIndex,isReliable,variantIndex,quantity}
 
     //String payload
+    STREAM_PUBLISH_TOKEN: 'stream publish token',
+    STREAM_UPDATE_PUBLISH_TOKEN: 'stream updates publish token',
+    SELLER_END_STREAM: 'seller ends stream',
+    SELLER_GET_PUBLISH_TOKEN: 'seller gets publish token',
+    SELLER_PUBLISH_PLAYER_STATUS: 'sellers publish player status', //"event_code",
+    SELLER_SET_CURRENT_PRODUCT_INDEX: 'seller sets current product index', //Product index
+    SELLER_START_STREAM: 'seller starts stream',
     USER_JOIN_STREAM: 'user joins stream',
     USER_ADD_MESSAGE: 'user adds chat message',
-    SELLER_START_STREAM: 'seller starts stream',
-    SELLER_END_STREAM: 'seller ends stream',
-    STREAM_PUBLISH_TOKEN: 'stream publish token',
-    SELLER_SET_CURRENT_PRODUCT_INDEX: 'seller sets current product index', //Product index
-    SELLER_GET_PUBLISH_TOKEN: 'seller gets publish token',
-    STREAM_UPDATE_PUBLISH_TOKEN: 'stream updates publish token',
-    SELLER_PUBLISH_PLAYER_STATUS: 'sellers publish player status' //"event_code",
 }
