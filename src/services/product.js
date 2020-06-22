eb91f0b2d06ac8d367433d5da74175ce2e1f0820
tmn57 <minhnhattranw@gmail.com>
2020-06-22 09:43:33 +0700

    minor updates

--- src/services/product.js
+++ src/services/product.js
@@ -1,15 +1,19 @@
-const socketStorage = require('../sockets/storage')
+const { streamSessions } = require('../sockets/services')
 const { emitToStream } = require('../sockets/io')
 const realTimeEventKeys = require('../sockets/event_keys.io')
 //input: product db object
-//output: streamId if have stream is live for product / return "" if none
+//output: {streamId, streamPrice} if have stream is live for product / return null if none
 const checkProductLiveStream = productDbObject => {
-    const { inStreams } = productDbObject
+    const { _id, inStreams } = productDbObject
     if (Array.isArray(inStreams) && inStreams.length) {
+        const productId = _id.toString()
         const streamId = inStreams[inStreams.length - 1]
-        if (socketStorage.streamSessions.has(streamId)) return streamId
+        if (streamSessions.has(streamId)) {
+            const { streamPrice } = streamSessions.get(streamId).products[productId]
+            return { streamId, streamPrice }
+        }
     }
-    return ""
+    return null
 }
 
 //For APIs: This function is triggered AFTER SUCCESSFUL product's variant's quantity update!!!
@@ -21,8 +25,8 @@ const checkProductLiveStream = productDbObject => {
 const onChangeQuantityProductVariant = (productId, variantIndex, newQuantity, inStreams) => {
     if (Array.isArray(inStreams) && inStreams.length) {
         const streamId = inStreams[inStreams.length - 1]
-        if (socketStorage.streamSessions.has(streamId)) {
-            emitToStream(streamId, realTimeEventKeys.STREAM_PRODUCT_QUANTITY, {productId, variantIndex, newQuantity})
+        if (streamSessions.has(streamId)) {
+            emitToStream(streamId, realTimeEventKeys.STREAM_PRODUCT_QUANTITY, { productId, variantIndex, newQuantity })
         }
     }
 }
