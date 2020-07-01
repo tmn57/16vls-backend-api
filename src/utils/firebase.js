851daa262ad6c74d26e9cf7c971b55f07e4b55a9
tmn57 <minhnhattranw@gmail.com>
2020-07-01 15:46:58 +0700

    update stream notifications

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -29,6 +29,7 @@ const sendMulticast = async (registrationTokens, messageObject) => {
     let msgObj = messageObject
     msgObj['tokens'] = registrationTokens
     console.log(`fb send multicast msgObj`, msgObj)
+    msgObj.notification.meta = msgObj['data']
     delete msgObj['data']
     await admin.messaging().sendMulticast(msgObj)
         .then((response) => {
