ff8a1331afb90752569b30a80bfc61de918de661
tmn57 <minhnhattranw@gmail.com>
2020-07-13 14:10:18 +0700

    update

--- src/sockets/io.js
+++ src/sockets/io.js
@@ -151,6 +151,7 @@ const initIoServer = server => {
                     return cb({ success: false, message: 'Sản phẩm nằm ngoài danh mục đang phát, có thể xuất phát từ lỗi ứng dụng', errorCode: 0 })
                 }
 
+
                 const inStreamAt = convertRealTimeToVideoTime(strm.streamId, Date.now())
                 strm['currentProductIndex'] = productIndex
                 strm.products[productIndex].inStreamAts.push(inStreamAt)
@@ -352,6 +353,7 @@ const updateLikedUsers = (streamId, userId, isUnlike) => {
 const convertRealTimeToVideoTime = (streamId, time) => {
     const stream = streamSessions.get(streamId)
     if (stream) {
+        console.log(`convertRealTimeToVideoTime called; videoStreamStatusHistory:`, videoStreamStatusHistory);
         const history = stream.videoStreamStatusHistory
         if (history.length <= 1) return -1
         if (history[1].statusCode !== StreamVideoStatus.START) return -1
