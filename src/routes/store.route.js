77d5f973681d6e7908c086573f4bdd43023164ea
Nguyễn Văn Nhật <1612457@student.hcmus.edu.vn>
2020-07-14 15:28:31 +0700

    update api

--- src/routes/store.route.js
+++ src/routes/store.route.js
@@ -295,6 +295,13 @@ router.post('/follow', asyncHandler(async (req, res, next) => {
   const user = await User.findById(userId)
   const store = await Store.findById(storeId)
 
+  if (store.userId === userId) {
+    return res.status(400).json({
+      success: false,
+      message: "Không thể theo dõi chính cửa hàng mình"
+    })
+  }
+
   let removeFollow = false;
   for (let i = 0; i < user.storeFollowed.length; i++) {
     if (user.storeFollowed[i] == storeId) {
