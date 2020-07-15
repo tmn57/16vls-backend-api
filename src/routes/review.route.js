c815cdd33274aa8acd6a7e7ed854ee3ea01fa85f
tmn57 <minhnhattranw@gmail.com>
2020-07-15 21:00:49 +0700

    update review feature

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -29,7 +29,7 @@ router.post('/', asyncHandler(async (req, res, next) => {
         if (user && user.isEnabled) {
             list.push({
                 ...r.toObject(),
-                userName: user.name
+                username: user.name
             })
         }
     }))
